e22c26151b2a377d29fec56c35cb049ecbe757f3
bebehr <behr.benny@gmail.com>
2024-09-21 17:28:38 +0200

    Add phpunit testing

--- tests/unit/FrozenClockTest.php
+++ tests/unit/FrozenClockTest.php
@@ -2,17 +2,86 @@
 
 declare(strict_types=1);
 
-require dirname(__DIR__, 2) . '/vendor/autoload.php';
+namespace Bebehr\Clock\Tests\Unit;
 
 use Bebehr\Clock\FrozenClock;
+use PHPUnit\Framework\TestCase;
+use Psr\Clock\ClockInterface;
 
-$clock = new FrozenClock(new DateTimeImmutable(
-    'now',
-    new DateTimeZone('Europe/Berlin'),
-));
+/**
+ * @covers \Bebehr\Clock\FrozenClock
+ */
+final class FrozenClockTest extends TestCase
+{
+    private \DateTimeZone $timeZone;
+    private \DateTimeImmutable $now;
+    private ClockInterface $clock;
 
-$dateTime = $clock->now();
-dump($dateTime);
+    protected function setUp(): void
+    {
+        $this->timeZone = new \DateTimeZone('Europe/Berlin');
+        $this->now = new \DateTimeImmutable('now', $this->timeZone);
+        $this->clock = new FrozenClock($this->now);
+    }
 
-$timeStamp = $clock->now()->getTimestamp();
-dump($timeStamp);
+    public function testClockIsInstanceOfFrozenClock(): void
+    {
+        $clock = $this->clock;
+
+        $this->assertInstanceOf(FrozenClock::class, $clock);
+    }
+
+    public function testClockImplementsClockInterface(): void
+    {
+        $clock = $this->clock;
+
+        $this->assertInstanceOf(ClockInterface::class, $clock);
+    }
+
+    public function testDateTimeInjection(): void
+    {
+        $now = $this->now;
+
+        $clock = new FrozenClock($now);
+        $nowReflection = (new \ReflectionClass($clock))
+            ->getProperty('now')
+        ;
+        $privateNow = $nowReflection->getValue($clock);
+
+        $this->assertSame($now, $privateNow);
+    }
+
+    public function testNowReturnsDateTimeimmutable(): void
+    {
+        $clock = $this->clock;
+
+        $now = $clock->now();
+
+        $this->assertInstanceOf(\DateTimeImmutable::class, $now);
+    }
+
+    public function testNowReturnsFreshDateTimeimmutable(): void
+    {
+        $clock = $this->clock;
+        $now = [];
+
+        $now[] = $clock->now();
+        $now[] = $clock->now();
+
+        $this->assertNotSame($now[1], $now[0]);
+    }
+
+    public function testNowReturnsNowProperty(): void
+    {
+        $clock = $this->clock;
+        $nowReflection = (new \ReflectionClass($clock))
+            ->getProperty('now')
+        ;
+        $privateNow = $nowReflection->getValue($clock);
+
+        sleep(1);
+        $now = $clock->now();
+
+        $this->assertEquals($privateNow, $now);
+    }
+}
--- tests/unit/SystemClockTest.php
+++ tests/unit/SystemClockTest.php
@@ -2,14 +2,84 @@
 
 declare(strict_types=1);
 
-require dirname(__DIR__, 2) . '/vendor/autoload.php';
+namespace Bebehr\Clock\Tests\Unit;
 
 use Bebehr\Clock\SystemClock;
+use PHPUnit\Framework\TestCase;
+use Psr\Clock\ClockInterface;
 
-$clock = new SystemClock();
+/**
+ * @covers \Bebehr\Clock\SystemClock
+ */
+final class SystemClockTest extends TestCase
+{
+    private \DateTimeZone $timeZone;
+    private ClockInterface $clock;
 
-$dateTime = $clock->now();
-dump($dateTime);
+    protected function setUp(): void
+    {
+        $this->timeZone = new \DateTimeZone('Europe/Berlin');
+        $this->clock = new SystemClock();
+    }
 
-$timeStamp = $clock->now()->getTimestamp();
-dump($timeStamp);
+    public function testClockIsInstanceOfSystemClock(): void
+    {
+        $clock = $this->clock;
+
+        $this->assertInstanceOf(SystemClock::class, $clock);
+    }
+
+    public function testClockImplementsClockInterface(): void
+    {
+        $clock = $this->clock;
+
+        $this->assertInstanceOf(ClockInterface::class, $clock);
+    }
+
+    public function testNowReturnsDateTimeimmutable(): void
+    {
+        $clock = $this->clock;
+
+        $now = $clock->now();
+
+        $this->assertInstanceOf(\DateTimeImmutable::class, $now);
+    }
+
+    public function testNowReturnsFreshDateTimeimmutable(): void
+    {
+        $clock = $this->clock;
+        $now = [];
+
+        $now[] = $clock->now();
+        $now[] = $clock->now();
+
+        $this->assertNotSame($now[1], $now[0]);
+    }
+
+    public function testNowReturnsCurrentDateTime(): void
+    {
+        $timeZone = $this->timeZone;
+        $clock = $this->clock;
+
+        $before = new \DateTimeImmutable('now', $timeZone);
+        $now = $clock->now();
+        $after = new \DateTimeImmutable('now', $timeZone);
+
+        $this->assertGreaterThanOrEqual($before, $now);
+        $this->assertLessThanOrEqual($after, $now);
+    }
+
+    public function testNowReturnsGreaterDateTimeAfterSecond(): void
+    {
+        $clock = $this->clock;
+
+        $before = $clock->now();
+        sleep(1);
+        $now = $clock->now();
+        sleep(1);
+        $after = $clock->now();
+
+        $this->assertGreaterThan($before, $now);
+        $this->assertLessThan($after, $now);
+    }
+}
